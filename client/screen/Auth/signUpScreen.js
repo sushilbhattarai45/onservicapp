076e70cf80f73c6db9518e283704a536399508a3
Sanskar <sskrlmsl@gmail.com>
2022-10-07 13:49:24 +0545

    a

--- client/screen/Auth/signUpScreen.js
+++ client/screen/Auth/signUpScreen.js
@@ -25,7 +25,8 @@ const gender = [
   { value: "Other", label: "Other" },
 ];
 
-const BASE_OUR_API_URL = "http://192.168.16.101:3001";
+// const BASE_OUR_API_URL = "http://192.168.16.101:3001";
+const BASE_OUR_API_URL = "http://192.168.18.7:3001";
 
 export default function SignUpScreen() {
   const [toggleCheckBox, setToggleCheckBox] = useState(false);
