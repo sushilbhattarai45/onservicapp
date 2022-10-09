c37968387856bc766476003de02050c759f7f7fd
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2022-10-09 17:06:49 +0545

    ok

--- client/component/imageSlider.js
+++ client/component/imageSlider.js
@@ -6,7 +6,6 @@ Text,
 Dimensions,
 Image
 } from 'react-native';
-import ViewSlider from 'react-native-view-slider'
 import { ImageSlider } from "react-native-image-slider-banner";
 
 const { width, height } = Dimensions.get('window');
