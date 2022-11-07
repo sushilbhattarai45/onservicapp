d107fd1af955784caa5cafd8522e96e781ea9c62
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2022-11-07 21:47:32 +0545

    review bug fixed plus search person listing fixed+complete auth

--- server/index.js
+++ server/index.js
@@ -21,11 +21,8 @@ App.listen(3001, () => {
   console.log("listening on port " + PORT);
 });
 App.use("/uploads", Express.static("uploads"));
-// App.use("/", (req, res) => {
-//   res.send("ok");
-// });
-App.use("/v1/api/", (req, res) => {
-  res.send(hello);
+App.use("/", (req, res) => {
+  res.send("ok");
 });
 
 App.use("/v1/api/user", user_route);
