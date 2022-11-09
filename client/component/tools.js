d49751846ef32f2226b35e71d9327f29cd56cfec
Sanskar <sskrlmsl@gmail.com>
2022-11-09 20:15:59 +0545

    add

--- client/component/tools.js
+++ client/component/tools.js
@@ -19,9 +19,10 @@ function deg2rad(deg) {
   return deg * (Math.PI / 180);
 }
 export const BASE_OUR_API_URL = "http:/192.168.18.7:3001";
+// export const BASE_OUR_API_URL = "http://192.168.100.11:3001";
+
 export const axiosInstance = axios.create({
-  baseURL: "http://192.168.18.7:3001/v1/api/",
-  // baseURL: "http://192.168.100.11:3001/v1/api/",
+  baseURL: BASE_OUR_API_URL + "/v1/api/",
 
   // baseURL: URL,
   headers: { "Content-Type": "application/json" },
