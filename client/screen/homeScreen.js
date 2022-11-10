61ac57f1784ad90e57fc0dec4e84bbe0a0483c1f
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2022-11-10 20:02:40 +0545

    review bug fixed plus search person listing fixed+complete auth

--- client/screen/homeScreen.js
+++ client/screen/homeScreen.js
@@ -50,7 +50,7 @@ const HomeScreen = ({ navigation }) => {
       let newaddons = await axiosInstance.post("/categories/newaddons", {
         GIVEN_API_KEY: "AXCF",
       });
-      console.log(featuredOnHome.data)
+      console.log(featuredOnHome.data);
       if (!featuredOnHome.error) setFeatured(featuredOnHome.data);
       if (!newaddons.error) setNewaddons(newaddons.data.data);
 
@@ -60,7 +60,6 @@ const HomeScreen = ({ navigation }) => {
         console.error(newaddons?.error);
       }
     }
-    
   }, []);
 
   return (
