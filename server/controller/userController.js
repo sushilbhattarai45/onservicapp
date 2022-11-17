b515d01266f1b1e4a7cd6a7fe3ba0384746c514d
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2022-11-17 22:49:53 +0545

    ok

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -2,6 +2,7 @@ import userSchema from "../model/userSchema.js";
 import multer from "multer";
 import {} from "dotenv/config";
 const API_KEY = process.env.API_KEY;
+import bcrypt from "bcrypt";
 import spSchema from "../model/spSchema.js";
 export const updateUser = async (req, res) => {
   const {
@@ -12,7 +13,6 @@ export const updateUser = async (req, res) => {
     user_city,
     user_street,
     user_gender,
-    user_password,
     user_status,
     user_toc,
     user_profileImage,
@@ -33,18 +33,20 @@ export const updateUser = async (req, res) => {
           user_city: user_city,
           user_street: user_street,
           user_gender: user_gender,
-          user_password: user_password,
           user_profileImage: user_profileImage,
         }
       );
       const updatedData = await userSchema.find({
         user_contact: user_contact,
       });
-      
-      const spUpdate = await spSchema.findOneAndUpdate({ sp_contact: user_contact }, {
-  sp_profileImage:user_profileImage
-})
-console.log(spUpdate)
+
+      const spUpdate = await spSchema.findOneAndUpdate(
+        { sp_contact: user_contact },
+        {
+          sp_profileImage: user_profileImage,
+        }
+      );
+      console.log(spUpdate);
 
       return res.json({ statuscode: 201, user: updatedData });
     }
@@ -70,9 +72,22 @@ export const registerUser = async (req, res) => {
     user_profileImage,
   } = req.body;
   try {
+    const password = bcrypt.hashSync(user_password, 10);
+    console.log(password);
     const exists = await userSchema.findOne({ user_contact: user_contact });
     if (!exists || exists?.length == 0) {
-      const user = new userSchema(req.body);
+      const user = new userSchema({
+        user_name: user_name,
+        user_email: user_email,
+        user_contact: user_contact,
+        user_district: user_district,
+        user_city: user_city,
+        user_street: user_street,
+        user_gender: user_gender,
+        user_password: password,
+        user_toc: user_toc,
+        user_profileImage: user_profileImage,
+      });
       const userData = await user.save();
       return res.json({ statuscode: 201, user: userData });
     } else {
@@ -112,16 +127,18 @@ export const loginUser = async (req, res) => {
   const { user_num, user_pass, GIVEN_API_KEY } = req.body;
   if (API_KEY == GIVEN_API_KEY) {
     try {
-      const loginUser = await userSchema.find({
+      const loginUser = await userSchema.findOne({
         user_contact: user_num,
-        user_password: user_pass,
       });
+
+      const password = bcrypt.compareSync(user_pass, loginUser?.user_password);
+      console.log(password);
       // return res.json({ data: loginUser });
-      if (loginUser.length != 0) {
+      if (password) {
         return res.json({
           statuscode: 200,
           message: "user found",
-          pass: user_pass,
+          pass: password,
           num: user_num,
         });
       } else {
@@ -155,12 +172,15 @@ export const getAllUser = async (req, res) => {
 };
 export const forgetPin = async (req, res) => {
   const { GIVEN_API_KEY, user_contact, user_password } = req.body;
+
+  const password = bcrypt.hashSync(user_password, 10);
+
   if (API_KEY == GIVEN_API_KEY) {
     try {
       const updated = await userSchema.findOneAndUpdate(
         { user_contact: user_contact },
         {
-          user_password: user_password,
+          user_password: password,
         }
       );
       return res.json({ statuscode: 201, data: updated });
