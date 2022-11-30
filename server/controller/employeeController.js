cc83dc2c95e667d05563cd415e0930a36c2d949c
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2022-11-30 22:08:19 +0545

    ok

--- server/controller/employeeController.js
+++ server/controller/employeeController.js
@@ -4,6 +4,31 @@ import adsSchema from "../model/adsSchema.js";
 import employeeSchema from "../model/employeSchema.js";
 const API_KEY = process.env.API_KEY;
 
+function getSms(otp, num) {
+  var url = "https://sms.aakashsms.com/sms/v3/send/";
+
+  var data = {
+    to: num,
+    auth_token:
+      "b83027e50e5ebe14738201708e8488ded718f4f139a51dbdd255264af88db89d",
+    text: " Hello User Your code is: " + otp + " Regards OnServic",
+  };
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      alert("Error" + error);
+    });
+}
+
 export const postEmployee = async (req, res) => {
   const {
     GIVEN_API_KEY,
@@ -180,21 +205,7 @@ export const login = async (req, res) => {
       });
       if (loginUser) {
         let genOtp = Math.floor(100000 + Math.random() * 900000);
-        var url = "https://sms.aakashsms.com/sms/v3/send/";
-
-        var data = {
-          to: employee_contact,
-          auth_token:
-            "b83027e50e5ebe14738201708e8488ded718f4f139a51dbdd255264af88db89d",
-          text: " Hello User Your code is: " + genOtp + " Regards OnServic",
-        };
-        fetch(url, {
-          method: "POST",
-          headers: {
-            "Content-type": "application/json; charset=UTF-8",
-          },
-          body: JSON.stringify(data),
-        });
+        getSms(genOtp, employee_contact);
         return res.json({
           statuscode: 201,
           otp: genOtp,
