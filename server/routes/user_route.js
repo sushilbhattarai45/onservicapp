410cbeb7fb6a95af7f91cc57b6b8f60adf81afbe
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2022-12-01 19:04:59 +0545

    ok

--- server/routes/user_route.js
+++ server/routes/user_route.js
@@ -36,5 +36,22 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+//Img Url Route
+router.post("/web/upload", upload.single("pic"), async (req, res) => {
+  try {
+    if (req.file === undefined)
+      return res
+        .status(400)
+        .json({ error: false, msg: "You must select a file." });
+    const url =
+      req.protocol + "://" + req.get("host") + "/uploads/" + req.file.filename;
+    res
+      .status(200)
+      .json({ error: false, msg: "File uploaded successfully!", imgUrl: url });
+  } catch (error) {
+    res.status(400).json({ error: true, msg: "Img upload failed" });
+  }
+});
+
 router.route("/uploadImage").post(upload.single("profile"), uploadImage);
 export default router;
